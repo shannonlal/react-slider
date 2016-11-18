a36a7eccd59f3d8672cb3e5d8557afb8a87529fc
Shannon Lal <shannonlal@hotmail.com>
2016-11-17 21:53:40 -0500

    Latest index

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Slider from './components/Slider';
+
+import Swiper from './components/Swiper';
 
 ReactDOM.render(
-  <Slider />, document.getElementById('app')
+  <Swiper />, document.getElementById('app')
 ); 
